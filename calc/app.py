27ecb914c5a7c5db982566cf517a1db5f66cb5d3
Liviu Grozav <grozlivj@gmail.com>
2023-11-16 00:43:17 -0800

    All Challenges Completed

--- calc/app.py
+++ calc/app.py
@@ -1,3 +1,49 @@
-from flask import Flask
-
+from operation import add, sub, multi, div
+from flask import Flask, request
 app = Flask(__name__)
+
+
+@app.route('/add')
+def func1():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+    return str(result)
+
+
+@app.route('/sub')
+def func2():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+    return str(result)
+
+
+@app.route('/multi')
+def func3():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = multi(a, b)
+    return str(result)
+
+
+@app.route('/div')
+def func4():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+    return str(result)
+
+
+@app.route('/math/<operation>')
+def calculate(operation):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    dict = {
+        'add': add,
+        'sub': sub,
+        'multi': multi,
+        'div': div
+    }
+    answer = dict[operation](a, b)
+    return str(answer)
--- greet/app.py
+++ greet/app.py
@@ -1,3 +1,18 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/welcome')
+def welcome():
+    return "Welcome"
+
+
+@app.route('/welcome/home')
+def welcome():
+    return "Welcome home"
+
+
+@app.route('/welcome/back')
+def welcome():
+    return "Welcome back"
